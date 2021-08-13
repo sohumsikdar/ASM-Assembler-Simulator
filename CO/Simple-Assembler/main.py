fead590e619a1ac204c2509009acf870ef9d5b91
Sohum Sikdar <pr0hum@Jalebi-Boy.local>
2021-08-13 18:42:01 +0530

    Added relevant functions in main

--- CO/Simple-Assembler/main.py
+++ CO/Simple-Assembler/main.py
@@ -3,16 +3,25 @@ from OPCode import *
 from typetable import *
 
 PC = 1
-SymbList = []
+SymbList = {}
 commandList = []
 
+def get8bit (k):
+    s = '{0:08b}'.format(k)
+    return s
+
+def giveOut(OP, lst):
+    pass
+
+# Assigns the Symbol/var none and returns bool to append the command
 def valid(lst):
     if(lst[0] != 'var'):
         return True
     global SymbList
-    SymbList.append(lst[1])
+    SymbList[lst[1]] = None
     return False
 
+# Storing the input lines in different lists
 while True:
     try:
         lst = input().split()
@@ -21,6 +30,13 @@ while True:
     except EOFError:
         break
 
-print(commandList)
-print(len(commandList))
+# Giving the values to the Symbols/var
+ctr = len(commandList)
+for k in SymbList.keys():
+    SymbList[k] = ctr
+    ctr += 1
+
+for i,cmd in enumerate(commandList):
+    
+
 print(SymbList)
