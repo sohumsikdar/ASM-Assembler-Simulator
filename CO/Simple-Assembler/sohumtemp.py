223be52294cd4e6dad1871f362bf9f3cdad774b9
Ritwick Pal <pal.ritwick0501@gmail.com>
2021-08-13 20:14:51 +0530

    Sohum's code

--- CO/Simple-Assembler/sohumtemp.py
+++ CO/Simple-Assembler/sohumtemp.py
@@ -12,14 +12,30 @@ from typetable import *
 
 
 
+def get8bit (k):
+    s = '{0:08b}'.format(k)
+    return s
+  
 def printCmd(cmd):
     op = cmd[0]
     s = ''
     if(op == "add"):
         s = opCode[op][0] + '00' + reg[cmd[1]] + reg[cmd[2]] + reg[cmd[3]]
-        print (s)
+    if(op == "sub"):
+        s = opCode[op][0] + '00' + reg[cmd[1]] + reg[cmd[2]] + reg[cmd[3]]
+    if(op == "mov"):
+        if(cmd[-1][0] == '$'):
+            k = int(cmd[-1][1:])
+            s = "00010" + reg[cmd[1]] + get8bit(k)
+        else :
+            s = "00011" + "00000" + reg[cmd[1]] + reg[cmd[2]]
+    if(op == "mul"):
+        s = opCode[op][0] + '00' + reg[cmd[1]] + reg[cmd[2]] + reg[cmd[3]]
+    if(op == 'ld'):
+        s = opCode[op] + reg[cmd[1]] + get8bit(SymbList[cmd[2]])
+    if(op == 'st'):
+        s = opCode[op] + reg[cmd[1]] + get8bit(SymbList[cmd[2]])
+    print(s)
 
 lst = input().split()
 printCmd(lst)
-
-# 00000 00 000 001 010
