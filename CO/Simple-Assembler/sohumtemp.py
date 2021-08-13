996292e37b088e2ecde8b540be8b46e5cd43d3a9
Sohum Sikdar <pr0hum@Jalebi-Boy.local>
2021-08-13 19:00:22 +0530

    added initial print statements

--- CO/Simple-Assembler/sohumtemp.py
+++ CO/Simple-Assembler/sohumtemp.py
@@ -1,3 +1,7 @@
+import sys
+from OPCode import *
+from typetable import *
+
 # "add" : ("00000", "A"),
 # "sub" : ("00001", "A"),
 # "movI" : ("00010", "B", ),
@@ -5,3 +9,17 @@
 # "ld" : ("0010", "D"),
 # "st" : ("00101", "D"),
 # "mul" : ("00110", "A"),
+
+
+
+def printCmd(cmd):
+    op = cmd[0]
+    s = ''
+    if(op == "add"):
+        s = opCode[op][0] + '00' + reg[cmd[1]] + reg[cmd[2]] + reg[cmd[3]]
+        print (s)
+
+lst = input().split()
+printCmd(lst)
+
+# 00000 00 000 001 010
