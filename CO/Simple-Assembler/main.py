e74ff14ccabea53b66f52af92cc4aa594b4a4620
Sohum Sikdar <pr0hum@Jalebi-Boy.local>
2021-08-13 23:16:46 +0530

    Ran the non erroneous test cases

--- CO/Simple-Assembler/main.py
+++ CO/Simple-Assembler/main.py
@@ -1,8 +1,6 @@
 import sys
-from OPCode import *
-from typetable import *
+from dic import *
 from commandFile import *
-from symboltable import *
 
 PC = 1
 commandList = []
@@ -24,8 +22,11 @@ while True:
         lst = input().split()
         if(valid(lst) == True):
             commandList.append(lst)
+
     except EOFError:
         break
+
+
 
 # Giving the values to the Symbols/var
 ctr = len(commandList)
@@ -33,6 +34,14 @@ for k in SymbList.keys():
     SymbList[k] = ctr
     ctr += 1
 
+# Giving the values to the labels
+
 for i,cmd in enumerate(commandList):
+    if((cmd[0][0:-1] and cmd[0]) not in opCode.keys()):
+        label[cmd[0][0:-1]] = get8bit(i)
+
+
+for i,cmd in enumerate(commandList):
+    if(cmd[0][-1] == ':'):
+        cmd = cmd[1:]
     printCmd(cmd)
-    
