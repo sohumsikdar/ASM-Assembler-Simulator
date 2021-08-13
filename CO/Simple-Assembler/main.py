2f42be7f639848e47df8cf069bec2c803e247826
Sohum Sikdar <pr0hum@Jalebi-Boy.local>
2021-08-13 16:31:04 +0530

    Made the Input file, Register Dict

--- CO/Simple-Assembler/main.py
+++ CO/Simple-Assembler/main.py
@@ -1,8 +1,26 @@
 import sys
 from OPCode import *
-import typetable
+from typetable import *
 
 PC = 1
 SymbList = []
+commandList = []
 
-complete_input = sys.stdin.read()
+def valid(lst):
+    if(lst[0] != 'var'):
+        return True
+    global SymbList
+    SymbList.append(lst[1])
+    return False
+
+while True:
+    try:
+        lst = input().split()
+        if(valid(lst) == True):
+            commandList.append(lst)
+    except EOFError:
+        break
+
+print(commandList)
+print(len(commandList))
+print(SymbList)
