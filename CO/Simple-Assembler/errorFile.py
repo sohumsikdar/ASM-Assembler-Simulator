dfd90917dacb417eff9143d1ef8644268cfd57f5
Sohum Sikdar <pr0hum@Jalebi-Boy.local>
2021-08-27 19:00:38 +0530

    Added line numbers for all errors

--- CO/Simple-Assembler/errorFile.py
+++ CO/Simple-Assembler/errorFile.py
@@ -6,28 +6,37 @@ from commandFile import *
 def iscmdvalid(cmd):
     op = cmd[0]
     if(op not in opCode.keys()):
+        print("Invalid OP Code ")
         return False
     t = opCode[op][1]
     
     if(op == "mov"):
         if(len(cmd) != 3):
+            print("Invalid syntax ")
             return False
 
         if(cmd[2][0] == '$'):
-            if(((cmd[1] not in reg.keys() or cmd[1] == "FLAGS")) or (int(cmd[2][1:]) not in range(0, 256))):
+            if(((cmd[1] not in reg.keys() or cmd[1] == "FLAGS"))):
+                print(cmd[1] + " not a register ")
+                return False
+            if(int(cmd[2][1:]) not in range(0, 256)):
+                print("Imm value not in range ")
                 return False
             return True
 
         elif(cmd[2] in reg.keys()):
             if(cmd[1] in reg.keys() and cmd[1] != "FLAGS"):
                 return True
+            print("Invalid register ")
             return False
 
         else:
+            print("Invalid syntax ")
             return False
 
     elif(t == 'A'):
         if(len(cmd) != 4):
+            print("Invalid syntax ")
             return False
 
         r1 = cmd[1]
@@ -35,52 +44,69 @@ def iscmdvalid(cmd):
         r3 = cmd[3]
 
         if((r1 not in reg.keys() or r1 == "FLAGS") or (r2 not in reg.keys() or r2 == "FLAGS") or (r3 not in reg.keys() or r3 == "FLAGS")):
+            print("Invalid register names ")
             return False
         return True
     
 
     elif(t == 'B'):
         if(len(cmd) != 3):
+            print("Invalid syntax ")
             return False
         r = cmd[1]
         if(cmd[2][0] != '$'):
+            print("Invalid syntax ")
             return False
 
         im = int(cmd[2][1:])
-        if((r not in reg.keys() or r == "FLAGS") or im not in range (0, 256)):
+        if((r not in reg.keys() or r == "FLAGS")):
+            print("Invalid register names ")
+            return False
+        if im not in range (0, 256):
+            print("Imm value not in range ")
             return False
         return True
 
 
     elif(t == 'C'):
         if(len(cmd) != 3):
+            print("Invalid syntax ")
             return False
         r1 = cmd[1]
         r2 = cmd[2]
         if((r1 not in reg.keys() or r1 == "FLAGS") or (r2 not in reg.keys() or r2 == "FLAGS")):
+            print("Invalid register names ")
             return False
         return True
     
 
     elif(t == 'D'):
         if(len(cmd) != 3):
+            print("Invalid syntax ")
             return False
         r = cmd[1]
         var = cmd[2]
-        if((r not in reg.keys() or r == "FLAGS") or var not in SymbList.keys() or var in label.keys()):
+        if((r not in reg.keys() or r == "FLAGS")):
+            print("Invalid register names ")
+            return False
+        if var not in SymbList.keys() or var in label.keys():
+            print("Invalid use of variables ")
             return False
         return True
 
 
     elif(t == 'E'):
         if(len(cmd) != 2):
+            print("Invalid syntax ")
             return False
         var = cmd[1]
         if(var not in label.keys() or var in SymbList.keys()):
+            print("Invalid use of label ")
             return False
         return True
 
     else:
+        print("Invalid syntax ")
         return False
     
     
--- CO/Simple-Assembler/main.py
+++ CO/Simple-Assembler/main.py
@@ -3,36 +3,38 @@ from dic import *
 from commandFile import *
 from errorFile import *
 
-PC = 1
+PC = 0
 commandList = []
+lineArr = []
+linNum = 0
 
 def giveOut(OP, lst):
     pass
 
 # Assigns the Symbol/var none and returns bool to append the command
-def valid(lst):
+def valid(lst, i):
     if(lst[0] != 'var'):
         return True
 
     if(len(lst) == 1):
-        print("Error: no variable name was mentioned during declaration")
+        print("Error: no variable name was mentioned during declaration at line " + str(i))
         sys.exit()
 
     elif(lst[1] in SymbList.keys()):
-        print("Error: Redeclaration of a variable")
+        print("Error: Redeclaration of a variable at line " + str(i))
         sys.exit()
 
     if(len(commandList) != 0):
-        print("Error: Variable not declared at the beginning")
+        print("Error: Variable not declared at the beginning at line " + str(i))
         sys.exit()
 
     for char in lst[1]:
         if(not(char.isalnum() == True or char == "_")):
-            print("Error: invalid varible name")
+            print("Error: invalid varible name at line " + str(i))
             sys.exit()
 
     if(lst[1].isdigit()):
-        print("Error: purely numberic variable name")
+        print("Error: purely numberic variable name at line " + str(i))
         sys.exit()
 
     SymbList[lst[1]] = None
@@ -40,12 +42,14 @@ def valid(lst):
 
 # Storing the input lines in different lists
 while True:
+    linNum += 1
     try:
         lst = input().split()
         if(len(lst) == 0):
             continue
-        if(valid(lst) == True):
+        if(valid(lst, linNum) == True):
             commandList.append(lst)
+            lineArr.append(linNum)
 
     except EOFError:
         break
@@ -64,21 +68,21 @@ for i,cmd in enumerate(commandList):
     if((cmd[0][0:-1] and cmd[0]) not in (opCode.keys() or reg.keys())):
         l = cmd[0]
         if(l[-1] != ':'):
-            print("Error: invalid label in instruction " + str(i + 1))
+            print("Error: invalid label in instruction " + str(lineArr[i]))
             sys.exit()
         l = l[0:-1]
 
         for char in l:
             if(not(char.isalnum() == True or char == '_')):
-                print("Error: invalid label in instruction " + str(i + 1))
+                print("Error: invalid label in instruction " + str(lineArr[i]))
                 sys.exit()
         
         if(l.isdigit()):
-            print("Error: invalid label in instruction " + str(i + 1))
+            print("Error: invalid label in instruction " + str(lineArr[i]))
             sys.exit()
         
         if(cmd[0][0:-1] in label.keys()):
-            print("Error: Redeclaration of a label in instruction " + str(i + 1))
+            print("Error: Redeclaration of a label in instruction " + str(lineArr[i]))
             sys.exit()   
 
         label[cmd[0][0:-1]] = get8bit(i)
@@ -93,7 +97,7 @@ for i,cmd in enumerate(commandList):
     # Strip for flags
     if(cmd[0][-1] == ':'):
         if(len(cmd) == 1):
-            print("Error in instruction " + str(i + 1))
+            print("Error in instruction " + str(lineArr[i]))
             sys.exit()
         cmd = cmd[1:]
 
@@ -101,7 +105,7 @@ for i,cmd in enumerate(commandList):
     hltCheck = True
     if(cmd[0] == "hlt"):
         if(not(len(cmd) == 1 and i + 1 == len(commandList))):
-            print("Error in instruction " + str(i + 1))
+            print("Error in instruction " + str(lineArr[i]))
             break
         else:
             print("1001100000000000")
@@ -111,11 +115,11 @@ for i,cmd in enumerate(commandList):
     flag = iscmdvalid(cmd)
 
     if(flag == False):
-        print("Error in instruction " + str(i + 1))
+        print("Error in instruction at line " + str(lineArr[i]))
         break
     else:
         printCmd(cmd)
 
     if(i == len(commandList) - 1 and cmd != "hlt"):
-        print("Error: missing hlt instruction")
+        print("Error: missing hlt instruction at line " + str(lineArr[i]))
         sys.exit()
