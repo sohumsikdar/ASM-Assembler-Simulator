08d0620d842fb6213dbca2591fe7c8c26f63ea0e
sohumsikdar <89215237+sohumsikdar@users.noreply.github.com>
2021-12-10 01:55:52 +0530

    Delete ritwicktemp.py

--- CO/SimpleSimulator/ritwicktemp.py
+++
@@ -1,8 +0,0 @@
-# from matplotlib import pyplot as plt
-# from simMain import *
-# x = [i+1 for i in range(len(commandList))]
-# y_cord = y
-
-# plt.scatter(x,y_cord)
-# plt.tight_layout()
-# plt.show()
